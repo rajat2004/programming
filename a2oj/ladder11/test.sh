4b7be0b4583bc494527e8a45483531321faac951
Rajat Singhal <rajatsinghal564@gmail.com>
2020-03-01 22:42:24 +0530

    Add Travis testing for fun

---
+++ a2oj/ladder11/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Just for fun, to make sure all codes compile :)
+
+# Speed up the process
+# Env Var NUMJOBS overrides automatic detection
+if [[ -n $NUMJOBS ]]; then
+    MJOBS=$NUMJOBS
+elif [[ -f /proc/cpuinfo ]]; then
+    MJOBS=$(grep -c processor /proc/cpuinfo)
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	MJOBS=$(sysctl -n machdep.cpu.thread_count)
+else
+    MJOBS=4
+fi
+
+make -j $MJOBS
